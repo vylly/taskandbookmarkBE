c746cff7d9439c8446a03c7b299d47f551a3f73a
William Gounot <william.gounot@teleporthq.io>
2024-11-16 02:32:44 +0100

    fixes for docker

--- src/main.ts
+++ src/main.ts
@@ -7,6 +7,6 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.enableCors();
   console.log('listening on port: ', process.env.PORT);
-  await app.listen(process.env.PORT ?? 3000);
+  await app.listen(process.env.PORT ?? 3001);
 }
 bootstrap();
